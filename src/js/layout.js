215a1be7d77195188a371a0ed5ff5d9e3b2168c8
Minerva Dutta <dutta.minerva@gmail.com>
2022-11-30 10:23:38 -0800

    work from 11.29s class

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,35 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import {Home} from "./pages/home";
+import Planet from "./pages/planet";
+import Character  from "./pages/character";
+
+
+
 
 //create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+	// const basename = process.env.BASENAME || "";
 
 	return (
 		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
+			<BrowserRouter>
+				
+			
 					<Routes>
 						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
+						<Route path="/character/:character" element={<Character />} />
+						<Route path="/planet/:planet" element={<Planet />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
-					<Footer />
-				</ScrollToTop>
+					
 			</BrowserRouter>
 		</div>
 	);
 };
 
-export default injectContext(Layout);
+export default Layout;
